deeb617fd62b87e55a74612720c8dac90b2c2bcb
SKYTRASTER <ominator06@gmail.com>
2020-11-19 14:36:31 +0300

    смена

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MainComponent} from './main/main.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,MainComponent, FormComponent
   ],
   imports: [
     BrowserModule,
